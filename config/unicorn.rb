0c419b54a8ac309801d97e2ee745922d14c0044c
Johan Frølich <johanfrolich@gmail.com>
2015-01-29 19:53:22 +0100

    Added unicorn webserver

---
+++ config/unicorn.rb
@@ -0,0 +1,26 @@
+# config/unicorn.rb
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+
+    config['pool'] = ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
