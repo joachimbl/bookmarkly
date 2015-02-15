4f75fd063dd6588dca53afc277cbdbb9213849db
joachimbl <joachim.blicher@gmail.com>
2015-02-15 23:26:14 +0100

    masonry2

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -1,24 +1,14 @@
 $(document).ready(function(){
-
-  var columns    = 1,
-  setColumns = function() { columns =
-    $( window ).width() > 986 ? 4 :
-    $( window ).width() > 769 ? 3 :
-    $( window ).width() < 768 ? 1 :
-    1; };
-
-  setColumns();
-  $( window ).resize( setColumns );
-
   var $container = $('#masonry-container');
 
   $container.imagesLoaded( function(){
     $container.masonry({
       itemSelector: '.box',
       isAnimated: !Modernizr.csstransitions,
-      columnWidth:  function( containerWidth ) { return containerWidth / columns; }
     });
   });
 });
 
-
+$(window).resize(function(){
+  location.reload();
+});
