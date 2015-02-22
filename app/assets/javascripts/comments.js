dada84e37514c30d3a52022cb40fb0922da92216
Johan Frølich <johanfrolich@gmail.com>
2015-02-22 16:23:36 +0100

    Autofocus on comment fields

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,4 +1,4 @@
 $(document).on('click', '[data-comment="reply"]', function(event){
   event.preventDefault();
-  $(this).parent().children('[data-comment="form"]').toggleClass('hidden');
+  $(this).parent().children('[data-comment="form"]').toggleClass('hidden').find('[name="comment[body]"]').focus();
 });
