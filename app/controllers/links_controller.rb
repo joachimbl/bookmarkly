2a54b0ab54d2218f02af164f8a05814272ae6b03
Johan Frølich <johanfrolich@gmail.com>
2015-03-17 16:58:18 +0100

    Fixed form input for private on links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -91,7 +91,7 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def link_params
-    params.require(:link).permit(:url, :tag_list, :private_link)
+    params.require(:link).permit(:url, :tag_list, :private)
   end
 
   def link_create_params
