60c2486be0d91b6a6005f929095ef4998210b6be
Johan Frølich <johanfrolich@gmail.com>
2015-03-17 18:03:57 +0100

    Refactored likes partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_to_unless(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      super
+    else
+      link_to(name, options, html_options, &block)
+    end
+  end
 end
