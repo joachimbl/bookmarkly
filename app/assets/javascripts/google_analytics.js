dc1de6ff432f3380d67fa79119bf4ba86039e06f
joachimbl <joachim.blicher@gmail.com>
2015-04-05 13:15:53 +0200

    outbound links

---
+++ app/assets/javascripts/google_analytics.js
@@ -0,0 +1,17 @@
+/**
+* Function that tracks a click on an outbound link in Google Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label.
+*/
+var trackOutboundLink = function(url) {
+ ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+   function () {
+   document.location = url;
+   }
+ });
+}
+
+$(document).on('click', '[data-outbound-link]', function(event) {
+  var url = $(this).attr('href');
+  trackOutboundLink(url);
+});
